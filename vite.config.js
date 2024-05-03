384b04be3a8f92417bb948a8f50c036cb32191be
friwid <50137120+friwid@users.noreply.github.com>
2024-05-03 10:19:44 +0200

    Change deploy

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/music-review-portal/',
+  base: './',
   build: {
     outDir: './docs'
   }
