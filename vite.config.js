76eb9442bba010e452387e87b7fda3d20bc349bc
friwid <50137120+friwid@users.noreply.github.com>
2024-05-03 10:00:24 +0200

    Revert back to deploy strategy without workflows

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  base: '/music-review-portal/',
+  build: {
+    outDir: './docs'
   }
 })
