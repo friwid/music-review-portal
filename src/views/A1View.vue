49d88473e3f493ff87858fdb82cd29a2246fb23a
friwid <50137120+friwid@users.noreply.github.com>
2024-05-03 14:49:11 +0200

    Improve A1

--- src/views/A1View.vue
+++ src/views/A1View.vue
@@ -1,24 +1,69 @@
 <script setup></script>
 
 <template>
-  <div class="wrapper">
+  <div class="page">
     <header>
-      <nav><RouterLink to="/">&#8630;</RouterLink></nav>
+      <nav><RouterLink to="/" class="backButton">&#8617;</RouterLink></nav>
     </header>
-    <main></main>
+    <div class="wrapper">
+      <main>
+        <h1>Login</h1>
+        <form>
+          <label for="email">E-Mail:</label>
+          <input type="email" id="email" name="email" required />
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+          <button type="submit">Login</button>
+        </form>
+      </main>
+    </div>
     <footer></footer>
   </div>
 </template>
 
 <style scoped>
+.page {
+  font-family: Bahnschrift, 'DIN Alternate', 'Franklin Gothic Medium', 'Nimbus Sans Narrow',
+    sans-serif-condensed, sans-serif;
+  padding: 10px;
+}
+
+.backButton {
+  display: block;
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  border-radius: 15px;
+  font-size: 2em;
+  line-height: 1.3em;
+  background-color: rgb(15, 15, 15);
+  box-shadow:
+    black 0 0 10px,
+    inset orangered 0 0 20px;
+  transition:
+    color 1s ease-out,
+    box-shadow 1s ease-out;
+}
+.backButton:hover {
+  box-shadow:
+    black 0 0 10px 2px,
+    inset springgreen 0 0 20px;
+  transition:
+    color 0.2s ease-out,
+    box-shadow 0.2s ease-out;
+}
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 a,
 a:visited {
   text-decoration: none;
-  font-size: 3em;
-  font-weight: 100;
-  letter-spacing: 0.2em;
   color: orangered;
-  text-shadow: orangered 0 0 10px;
+  text-shadow: rgb(0, 0, 0) 0 0 4px;
   transition:
     color 1s ease-out,
     text-shadow 1s ease-out;
@@ -27,9 +72,18 @@ a:visited {
 a:hover,
 a:active {
   color: springgreen;
-  text-shadow: springgreen 0 0 30px;
+  text-shadow: rgb(0, 0, 0) 0 0 4px;
   transition:
     color 0.2s ease-out,
     text-shadow 0.2s ease-out;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+label,
+button {
+  margin-top: 10px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,61 @@
 <script setup></script>
 
 <template>
-  <div class="wrapper">
+  <div class="page">
     <header>
       <nav></nav>
     </header>
-    <main>
-      <RouterLink to="/a1">A1</RouterLink>
-      <RouterLink to="/a2"> A2 </RouterLink>
-      <RouterLink to="/a3"> A3 </RouterLink>
-      <RouterLink to="/a4">A4</RouterLink>
-    </main>
+    <div class="wrapper">
+      <main>
+        <RouterLink to="/a1" class="button">1</RouterLink>
+        <RouterLink to="/a2" class="button">2</RouterLink>
+        <RouterLink to="/a3" class="button">3</RouterLink>
+        <RouterLink to="/a4" class="button">4</RouterLink>
+      </main>
+    </div>
     <footer></footer>
   </div>
 </template>
 
 <style scoped>
-.wrapper {
-  line-height: 1.6;
+.page {
+  font-size: 1.5em;
   font-family: Bahnschrift, 'DIN Alternate', 'Franklin Gothic Medium', 'Nimbus Sans Narrow',
     sans-serif-condensed, sans-serif;
-  font-size: 12px;
-  font-weight: normal;
-  display: flex;
-  place-items: center;
 }
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.button {
+  display: inline-block;
+  margin: 0 10px;
+  padding-left: 11px;
+  width: 90px;
+  height: 90px;
+  text-align: center;
+  border-radius: 80px;
+  line-height: 1.78em;
+  background-color: rgb(15, 15, 15);
+  box-shadow:
+    black 0 0 10px,
+    inset orangered 0 0 50px;
+  transition:
+    color 1s ease-out,
+    box-shadow 1s ease-out;
+}
+.button:hover {
+  box-shadow:
+    black 0 0 15px 5px,
+    inset springgreen 0 0 50px;
+  transition:
+    color 0.2s ease-out,
+    box-shadow 0.2s ease-out;
+}
+
 a,
 a:visited {
   text-decoration: none;
@@ -32,7 +63,7 @@ a:visited {
   font-weight: 100;
   letter-spacing: 0.2em;
   color: orangered;
-  text-shadow: orangered 0 0 10px;
+  text-shadow: rgb(0, 0, 0) 0 0 6px;
   transition:
     color 1s ease-out,
     text-shadow 1s ease-out;
@@ -41,7 +72,7 @@ a:visited {
 a:hover,
 a:active {
   color: springgreen;
-  text-shadow: springgreen 0 0 30px;
+  text-shadow: rgb(0, 0, 0) 0 0 6px;
   transition:
     color 0.2s ease-out,
     text-shadow 0.2s ease-out;
