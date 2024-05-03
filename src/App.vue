cfccb127853df07cf33e79c9fad4853c7d0b8708
friwid <50137120+friwid@users.noreply.github.com>
2024-05-03 11:25:35 +0200

    Remove test text

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <span>App.vue</span>
   <RouterView />
 </template>
 
-<style scoped>
-span {
-  color: red;
-}
-</style>
+<style scoped></style>
