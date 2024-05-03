b6f6d3ebfc91240690a52f02b63cedd2b9426496
friwid <50137120+friwid@users.noreply.github.com>
2024-05-03 11:50:57 +0200

    Change test text

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <span>App.vue</span>
+  <span></span>
   <RouterView />
 </template>
 
 <style scoped>
 span {
-  color: red;
+  display: hidden;
 }
 </style>
