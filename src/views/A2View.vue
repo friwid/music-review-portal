ce13ee6c0d190a87712aa4c4b8b6d626e9612293
friwid <50137120+friwid@users.noreply.github.com>
2024-07-27 18:32:24 +0200

    Add A2-A4 views

---
+++ src/views/A2View.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <div class="page">
+    <header>
+      <nav><RouterLink to="/" class="backButton">&#8617;</RouterLink></nav>
+    </header>
+    <div class="wrapper">
+      <main></main>
+    </div>
+    <footer></footer>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  font-family: Bahnschrift, 'DIN Alternate', 'Franklin Gothic Medium', 'Nimbus Sans Narrow',
+    sans-serif-condensed, sans-serif;
+  padding: 10px;
+}
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
